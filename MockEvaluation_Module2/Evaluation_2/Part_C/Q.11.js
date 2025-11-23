2953220266c54f7a9b966f32829bc371112160e6
sani816 <saniakhatun622@gmail.com>
2025-11-23 21:01:26 +0530

    Add files via upload

--- MockEvaluation_Module2/Evaluation_2/Part_C/Q.11.js
+++ MockEvaluation_Module2/Evaluation_2/Part_C/Q.11.js
@@ -1,1 +1,16 @@
+function createWallet(){
+    let balance=0
+    return{
+        addMoney:function(amount){
+            balance=balance+amount
+            console.log("deposite:",amount)},
+        CheckBalance:function(amount){
+            console.log("after deposite all money total balance is:",balance)
+        }    
 
+        }
+    }
+let myWallet = createWallet();
+myWallet.addMoney(500);
+myWallet.addMoney(200);
+myWallet.CheckBalance();  // should show 700
